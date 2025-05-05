2601b64d2d543b27953edc3cb2603d211563d608
reposforlabs <160736679+reposforlabs@users.noreply.github.com>
2025-05-05 23:57:36 +0530

    Update and rename STEP1-PS-Execute.ps1 to STEP1-script.ps1

--- STEP1-PS-Execute.ps1
+++
@@ -1,5 +0,0 @@
-# Download the script
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/your-username/repo-name/main/script.ps1" -OutFile "script.ps1"
-
-# Execute it
-.\script.ps1
---
+++ STEP1-script.ps1
@@ -0,0 +1,5 @@
+# Download the script
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/reposforlabs/demo/refs/heads/main/deploy-terraform-container.ps1"
+
+# Execute it
+.\script.ps1
