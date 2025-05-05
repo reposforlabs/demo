3ac96f68117c74114815e9849fa39acf61a01bbb
reposforlabs <160736679+reposforlabs@users.noreply.github.com>
2025-05-05 23:59:36 +0530

    Update STEP1-script.ps1

--- STEP1-script.ps1
+++ STEP1-script.ps1
@@ -1,5 +1,5 @@
 # Download the script
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/reposforlabs/demo/refs/heads/main/deploy-terraform-container.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/your-username/repo-name/main/script.ps1" -OutFile "script.ps1"
 
 # Execute it
 .\script.ps1
