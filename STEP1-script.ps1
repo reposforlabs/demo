509111bcda0daea7188332f7896bb559d091d2f6
reposforlabs <160736679+reposforlabs@users.noreply.github.com>
2025-05-06 00:42:33 +0530

    Delete STEP1-script.ps1

--- STEP1-script.ps1
+++
@@ -1,5 +0,0 @@
-# Download the script
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/reposforlabs/demo/refs/heads/main/deploy-terraform-container.ps1" -OutFile "script.ps1"
-
-# Execute it
-.\script.ps1
